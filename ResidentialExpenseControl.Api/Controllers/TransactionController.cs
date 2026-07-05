dd0c8faa005445cb0b762b68cca1cb2fff677495
Pedro Carvalho <pedro8.carvalho@gmail.com>
2026-07-05 10:25:14 -0300

    Creating controller for the transaction

---
+++ ResidentialExpenseControl.Api/Controllers/TransactionController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ResidentialExpenseControl.Api.DTOs.Transaction;
+using ResidentialExpenseControl.Api.Services;
+
+namespace ResidentialExpenseControl.Api.Controllers;
+
+[ApiController]
+[Route("api/transactions")]
+public class TransactionController : ControllerBase
+{
+    private readonly TransactionService _transactionService;
+
+    public TransactionController(TransactionService transactionService)
+    {
+        _transactionService = transactionService;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<List<TransactionResponseDTO>>> GetAll()
+    {
+        var transactions = await _transactionService.GetAllAsync();
+
+        return Ok(transactions);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<TransactionResponseDTO>> Create(TransactionRequestDTO dto)
+    {
+        try
+        {
+            var transaction = await _transactionService.CreateAsync(dto);
+
+            return StatusCode(StatusCodes.Status201Created, transaction);
+        }
+        catch (ArgumentException exception)
+        {
+            return BadRequest(new
+            {
+                message = exception.Message
+            });
+        }
+        catch (InvalidOperationException exception)
+        {
+            return BadRequest(new
+            {
+                message = exception.Message
+            });
+        }
+    }
+}
